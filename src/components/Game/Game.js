359e23c73ac79182736d1249dc1dbfe4b2b933ff
Marcos Cunha Lima <marcos@futuranet.com.br>
2023-08-06 19:21:15 -0300

    inicio

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,8 +8,35 @@ const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
+const Erro = ({label}) => {
+  return (
+    <p> {label} </p>
+  )
+}
+
+
 function Game() {
-  return <>Put a game here!</>;
+
+  const [guess, setGuess] = React.useState('')
+  const [erro, setErro] = React.useState(false)
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    if (guess.length !== 5) {
+      setErro(true)
+    } else {
+      setErro(false)
+      console.log(guess)
+      setGuess('')
+    }
+  }
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input id="guess-input" style={erro ? { border: '4px solid red'} : null }  type="text" value={guess} onChange={ (evt) => { setGuess(evt.target.value.toUpperCase()) }}/>
+      {erro ? <Erro label={"Tamanho deve ser de exatamente 5 letras"} /> : null}
+    </form>
+  )
 }
 
 export default Game;
